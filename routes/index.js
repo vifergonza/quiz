bfca317e8d5572ffd24fd04ac71ef80620664982
Victor Fernandez <vfernandezg@VFERNANDEZGW7.indra.es>
2015-06-10 16:08:15 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller.js');
+
+console.log('VFG [router]');
+
 /* GET home page. */
 router.get('/', function(req, res) {
+  console.log('VFG [router, get]');
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
