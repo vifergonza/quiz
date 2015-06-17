b56a6af7c96b6e0afdca20dfd922c04abfb6f120
Victor Fernandez <vfernandezg@VFERNANDEZGW7.indra.es>
2015-06-17 17:41:40 +0200

    Adaptacion para Heroku

--- models/models.js
+++ models/models.js
@@ -3,10 +3,25 @@ var path = require('path');
 //Cargar modelo ORM	
 var sequelizeOrm = require('sequelize');
 
-//usar BBDD SQLite:
-var sequelizeBd = new sequelizeOrm(null, null, null, {
-    dialect: 'sqlite',
-    storage: 'quiz.sqlite'
+//Configuracion de entorno
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var dbName = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+//Cargar base de datos en funcion del entorno
+var sequelizeBd = new sequelizeOrm(dbName, user, pwd, {
+    dialect: dialect,
+    protocol: protocol,
+    port: port,
+    host: host,
+    storage: storage, //solo SqlLite
+    omitNull: true //solo posrtgree
 });
 
 //Importar la definicion de la tabla Quiz en quiz.js
@@ -16,17 +31,17 @@ var quizTabla = sequelizeBd.import(path.join(__dirname, 'Quiz'));
 exports.quiz = quizTabla;
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelizeBd.sync().then(function (){
-	//Ejecuta el manejador una vez creada la tabla
-	quizTabla.count().then(function(count){
-		if (0 === count) {
-			quizTabla.create({
-				pregunta: '¿Capital de Italia?',
-				 respuesta: 'Roma'
-			})
-			.then(function(){
-				console.log('Tabla Quiz inicializa');
-			});
-		}
-	});
+sequelizeBd.sync().then(function() {
+    //Ejecuta el manejador una vez creada la tabla
+    quizTabla.count().then(function(count) {
+        if (0 === count) {
+            quizTabla.create({
+                    pregunta: '¿Capital de Italia?',
+                    respuesta: 'Roma'
+                })
+                .then(function() {
+                    console.log('Tabla Quiz inicializa');
+                });
+        }
+    });
 });
